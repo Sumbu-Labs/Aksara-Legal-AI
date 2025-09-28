043bac961cae80d744e85e86969042a5671fd24c
Dzikran <sjdn.dzikran@gmail.com>
2025-09-28 15:22:40 +0700

    feat(page): Add problem and solution sections with content and separators

---
+++ frontend/src/components/SectionSeparator.tsx
@@ -0,0 +1,7 @@
+export function SectionSeparatorHorizontal() {
+  return <hr className="border-black border-2" />;
+}
+
+export function SectionSeparatorVertical() {
+  return <div className="border-l-2 border-black h-full"></div>;
+}
